d2e5c52cdd9c22a7ee85f1b90bf428404a54b9db
Tyuiu.MedvedevKA@outlook.com <medvedevkirill2016@mail.ru>
2023-12-14 21:34:58 +0500

    add

--- Tyuiu.MedvedevKA.Sprint7.Project.V6.Lib/DataService.cs
+++ Tyuiu.MedvedevKA.Sprint7.Project.V6.Lib/DataService.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using System.IO;
 namespace Tyuiu.MedvedevKA.Sprint7.Project.V6.Lib
 {
-    public class Patient
+    public class Patients
     {
         public int Id { get; set; }
         public string LastName { get; set; }
@@ -18,9 +18,9 @@ namespace Tyuiu.MedvedevKA.Sprint7.Project.V6.Lib
     {
         private const string PatientsFilePath = "Patients.csv";
 
-        public List<Patient> GetAllPatients()
+        public List<Patients> GetAllPatients()
         {
-            List<Patient> patients = new List<Patient>();
+            List<Patients> patients = new List<Patients>();
 
             using (StreamReader reader = new StreamReader(PatientsFilePath))
             {
@@ -31,7 +31,7 @@ namespace Tyuiu.MedvedevKA.Sprint7.Project.V6.Lib
 
                     if (values.Length == 5)
                     {
-                        Patient patient = new Patient();
+                        Patients patient = new Patients();
                         patient.Id = int.Parse(values[0]);
                         patient.LastName = values[1];
                         patient.FirstName = values[2];
@@ -46,5 +46,46 @@ namespace Tyuiu.MedvedevKA.Sprint7.Project.V6.Lib
             return patients;
         }
     }
-   
+    public class Doctors
+    {
+        public string LFMName { get; set; }
+        public string Specialization { get; set; }
+        public string Diagnosis { get; set; }
+        public string Treatment { get; set; }
+        public string LossOfEmployment { get; set; }
+        public string Accounting { get; set; }
+        public string Note { get; set; }
+    }
+
+   public class DoctorsRepository
+    {
+        private const string DoctorsFilePath = "Doctors.csv";
+        public List<Doctors> GetAllDoctors()
+        {
+            List<Doctors> doctors = new List<Doctors>();
+            using (StreamReader reader = new StreamReader(DoctorsFilePath))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] values = line.Split(';');
+
+                    if (values.Length == 7)
+                    {
+                        Doctors doctor = new Doctors();
+                        doctor.LFMName = values[0];
+                        doctor.Specialization = values[1];
+                        doctor.Diagnosis = values[2];
+                        doctor.Treatment = values[3];
+                        doctor.LossOfEmployment = values[4];
+                        doctor.Accounting = values[5];
+                        doctor.Note = values[6];
+
+                        doctors.Add(doctor);
+                    }
+                }
+            }
+            return doctors;
+        }
+    }
 }
--- Tyuiu.MedvedevKA.Sprint7.Project.V6.Test/DataServiceTest.cs
+++ Tyuiu.MedvedevKA.Sprint7.Project.V6.Test/DataServiceTest.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using Tyuiu.MedvedevKA.Sprint7.Project.V6.Lib;
+using System.IO;
 
 namespace Tyuiu.MedvedevKA.Sprint7.Project.V6.Test
 {
@@ -8,8 +9,20 @@ namespace Tyuiu.MedvedevKA.Sprint7.Project.V6.Test
     public class DataServiceTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CheckFilePatient()
         {
+            string path = @"C:\Users\1\source\repos\Tyuiu.MedvedevKA.Sprint7\Tyuiu.MedvedevKA.Sprint7.Project.V6\bin\Debug\Patients.csv";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExist = fileInfo.Exists;
+            Assert.AreEqual(true, fileExist);
+        }
+        [TestMethod]
+        public void CheckFileDoctors()
+        {
+            string path = @"C:\Users\1\source\repos\Tyuiu.MedvedevKA.Sprint7\Tyuiu.MedvedevKA.Sprint7.Project.V6\bin\Debug\Doctors.csv";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExist = fileInfo.Exists;
+            Assert.AreEqual(true, fileExist);
         }
     }
 }
