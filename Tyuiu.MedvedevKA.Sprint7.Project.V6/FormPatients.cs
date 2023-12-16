ed8d721cf11bf1a050418539af8a81b8ae6955c4
Tyuiu.MedvedevKA@outlook.com <medvedevkirill2016@mail.ru>
2023-12-17 00:18:57 +0500

    add

--- Tyuiu.MedvedevKA.Sprint7.Project.V6/FormPatients.cs
+++ Tyuiu.MedvedevKA.Sprint7.Project.V6/FormPatients.cs
@@ -25,31 +25,30 @@ namespace Tyuiu.MedvedevKA.Sprint7.Project.V6
         {
             string filePath = @"C:\Users\1\source\repos\Tyuiu.MedvedevKA.Sprint7\Tyuiu.MedvedevKA.Sprint7.Project.V6\bin\Debug\Patients.csv";
 
-            // Проверяем, существует ли файл
-            if (File.Exists(filePath))
-            {
-                // Читаем все строки из файла
-                string[] lines = File.ReadAllLines(filePath);
+            // Создание таблицы данных
+            DataTable dt = new DataTable();
 
-                // Устанавливаем количество строк и столбцов в DataGridView
-                dataGridViewOriginal_MKA.RowCount = lines.Length;
-                dataGridViewOriginal_MKA.ColumnCount = 5;
+            // Создание столбцов
+            dt.Columns.Add("Номер", typeof(int));
+            dt.Columns.Add("Фамилия", typeof(string));
+            dt.Columns.Add("Имя", typeof(string));
+            dt.Columns.Add("Отчество", typeof(string));
+            dt.Columns.Add("Дата рождения пациента", typeof(DateTime));
 
-                // Заполняем DataGridView данными из файла
-                for (int i = 0; i < lines.Length; i++)
-                {
-                    string[] values = lines[i].Split(',');
+            // Добавление строк
+            dt.Rows.Add(1, "Крючкова", "София", "Матвеевна", new DateTime(2000, 2, 6));
+            dt.Rows.Add(2, "Ефремов", "Сергей", "Иванович", new DateTime(1997, 8, 10));
+            dt.Rows.Add(3, "Булатов", "Андрей", "Константинович", new DateTime(2001, 11, 16));
+            dt.Rows.Add(4, "Иванов", "Лев", "Никитич", new DateTime(2002, 2, 26));
+            dt.Rows.Add(5, "Смирнова", "Полина", "Ярославна", new DateTime(1996, 9, 15));
+            dt.Rows.Add(6, "Белов", "Иван", "Иванович", new DateTime(2000, 1, 26));
+            dt.Rows.Add(7, "Попов", "Степан", "Денисович", new DateTime(1998, 4, 1));
+            dt.Rows.Add(8, "Грачев", "Александр", "Янович", new DateTime(1996, 1, 10));
+            dt.Rows.Add(9, "Киселева", "Евгения", "Романовна", new DateTime(1999, 8, 17));
+            dt.Rows.Add(10, "Кошелев", "Святослав", "Фёдорович", new DateTime(1998, 5, 7));
 
-                    for (int j = 0; j < values.Length; j++)
-                    {
-                        dataGridViewOriginal_MKA[j, i].Value = values[j];
-                    }
-                }
-            }
-            else
-            {
-                MessageBox.Show("Файл не найден!");
-            }
+            // Установка таблицы данных в DataGridView
+            dataGridViewOriginal_MKA.DataSource = dt;
         }
 
     }
